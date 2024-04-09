297f88e1ba7535f5280a14d738ec3da9ce60aba5
anza <anza@bbd.co.za>
2024-04-09 20:54:24 +0200

    using env variable for  ClientId

--- client/Global/ClientConfiguration.cs
+++ client/Global/ClientConfiguration.cs
@@ -4,7 +4,7 @@ namespace client.Global
 {
     public static class ClientConfiguration
     {
-        public const string ClientId = "";
+        public static string ClientId = Environment.GetEnvironmentVariable("CLIENT_ID") ?? "";
         public const string RedirectUri = "http://localhost";
         public const string AuthorizationEndpoint = "https://github.com/login/device/code";
         public const string TokenEndpoint = "https://github.com/login/oauth/access_token";
