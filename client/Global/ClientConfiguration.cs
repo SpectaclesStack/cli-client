38428d3eae0ed9a8d4a3339ef966bec81d470a64
anza <anza@bbd.co.za>
2024-04-09 19:07:06 +0200

    clean up

--- client/Global/ClientConfiguration.cs
+++ client/Global/ClientConfiguration.cs
@@ -5,14 +5,12 @@ namespace client.Global
     public static class ClientConfiguration
     {
         public const string ClientId = "";
-        //public const string ClientSecret = "your_client_secret";
         public const string RedirectUri = "http://localhost";
         public const string AuthorizationEndpoint = "https://github.com/login/device/code";
         public const string TokenEndpoint = "https://github.com/login/oauth/access_token";
         public const string Scope = "read:user";
         public const string GrantType = "urn:ietf:params:oauth:grant-type:device_code";
         public const string ApiDomain = "http://localhost:5033";
-        //public const string ApiDomain = "https://localhost:5001";
 
         public static string accessToken { get; set; }
         public static string user { get; set; }
