aecee7bf471da09c98196678c8ebebc663ce78ad
anza <anza@bbd.co.za>
2024-04-09 19:07:06 +0200

    clean up

---
+++ client/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace client.Models
+{
+    public class User
+    {
+        [JsonPropertyName("userId")]
+        public int UserId { get; set; }
+
+        [JsonPropertyName("userName")]
+        public string UserName { get; set; }
+
+        [JsonPropertyName("createAt")]
+        public DateTime CreateAt { get; set; }
+    }
+}
