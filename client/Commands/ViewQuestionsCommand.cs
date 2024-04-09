e471e049dbb4bc8b7128c0847f26097416e39c33
anza <anza@bbd.co.za>
2024-04-09 20:04:26 +0200

    additional commands and logic

---
+++ client/Commands/ViewQuestionsCommand.cs
@@ -0,0 +1,35 @@
+﻿using client.Global;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client.Commands
+{
+    public class ViewQuestionsCommand : Command
+    {
+        public ViewQuestionsCommand() : base("View all questions", "V")
+        {
+        }
+
+        public override bool execute()
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                HttpRequestMessage request = new HttpRequestMessage(
+                                HttpMethod.Get,
+                                $"{ClientConfiguration.ApiDomain}/api/questions"
+                                );
+                //request.Headers.Add("Authorization", ClientConfiguration.accessToken);
+
+                HttpResponseMessage response = httpClient.Send(request);
+
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+
+                return true;
+            }
+        }
+    }
+}
---
+++ client/Models/Question.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace client.Models
+{
+    public class Question
+    {
+        [JsonPropertyName("questionId")]
+        public int QuestionId { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("body")]
+        public string Body { get; set; }
+
+        [JsonPropertyName("createAt")]
+        public DateTime CreateAt { get; set; }
+
+        [JsonPropertyName("users")]
+        public User User { get; set; }
+    }
+}
